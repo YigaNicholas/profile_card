a1e8a1ad150d8a6e6811af880a3641236677715d
yiga nicholas <yiganicholas1@gmail.com>
2025-05-09 12:16:30 +0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Avartar from './components/Avartar';
+
+
+const App = () => {
+  return (
+    <div className='w-full min-h-screen bg-yellow-500 flex justify-center'>
+      hello world
+      <Avartar/>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Avartar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Avartar = () => {
+  return (
+    <div className='w-full min-h-screen bg-yellow-500 flex justify-center'>
+      avartar
+
+    </div>
+  );
+};
+
+export default Avartar;
